9c7c58c983763af6e943cfaaac0528c86242f2f7
w-kuipers <wibokuip@gmail.com>
2024-01-01 18:55:43 +0100

    Initialize day 4

---
+++ day-4/src/main.rs
@@ -0,0 +1,16 @@
+use std::fs;
+
+fn main() {
+    println!("Day 4 - Part 1: {}", part1())
+}
+
+fn part1() -> u32 {
+
+    let input_data = fs::read_to_string("input-simple.txt").expect("Unable to read input data"); 
+
+    println!("{}", input_data);
+
+    let mut answer: u32 = 0;
+
+    return answer
+}
